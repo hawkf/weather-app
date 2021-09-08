ccee1b019bf3bfd591a1f1b7245ab5be037dd32c
hawkf <fedor.gorokhov@gmail.com>
2021-09-08 19:41:08 +0300

    add page-not-found

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AppRoute } from '../../utils/const';
 import DayWeatherMain from '../day-weather-main/day-weather-main';
 import WeekWeatherMain from '../week-weather-main/week-weather-main';
+import { PageNotFound } from '../page-not-found/page-not-found';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route exact path={AppRoute.WEEK}>
           <WeekWeatherMain />
+        </Route>
+        <Route>
+          <PageNotFound />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/components/page-not-found/page-not-found.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function PageNotFound() {
+  return (
+    <main className='main-page'>
+      <h1 className='page-not-found__title'>404</h1>
+      <p className='page-not-found__text'>Page not found</p>
+    </main>
+  );
+}
